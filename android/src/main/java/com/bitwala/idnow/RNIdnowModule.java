9560007a8731e7936875948d80087231745c2b5d
Rohan Weligamage <rcweligamage@gmail.com>
2024-02-28 10:15:51 +0100

    locally works

--- android/src/main/java/com/bitwala/idnow/RNIdnowModule.java
+++ android/src/main/java/com/bitwala/idnow/RNIdnowModule.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
